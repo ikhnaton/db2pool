edfd4fefb9367a6de8af62cfdbc7491fa8d02a48
Niall Cargill <ikhnaton@us.ibm.com>
2017-09-14 08:44:58 -0700

    enhance pool by adding inital warmup and purgeability

--- dbPool.js
+++ dbPool.js
@@ -15,23 +15,7 @@ function dbPool()
 
 			if ((available == -1) && (pool[dbName].connections.length < pool[dbName].maxPoolSize))
 			{
-				var config = (process.env.DBPOOL != null)?JSON.parse(process.env.DBPOOL)[dbName]:require(__basedir + 'credentials/' + dbName + '.json');
-
-				ibmdb.open(_buildConectionString(config), function (err, conn)
-				{
-					if (err)
-					{
-						console.log(err);
-						reject(err);
-					}
-					else
-					{
-						pool[dbName].counter++;
-						pool[dbName].connections.push({ _connection: conn, active: false, id: pool[dbName].counter });
-						var dex = _.findIndex(pool[dbName].connections, { id: pool[dbName].counter });
-						resolve(pool[dbName].connections[dex]);
-					}
-				});
+				resolve(addNewConnection(dbName));
 			}
 			else if (available == -1)
 			{
@@ -53,7 +37,6 @@ function dbPool()
 	{
 		var promise = new Promise((resolve, reject) =>
 		{
-//			console.log(new Date(), connection.id);
 			connection.active = true;
 			connection._connection.query(queryString, bindingParameters).then((result) => {
 				connection.active = false;
@@ -70,18 +53,73 @@ function dbPool()
 
 	this.setPoolMax = function(dbName, size)
 	{
-		mi.init(dbName);
-		pool[dbName].maxPoolSize = size;
+		return new Promise((resolve, reject) =>
+		{
+			mi.init(dbName)
+				.then(success => {
+					pool[dbName].maxPoolSize = size;
+					resolve("success");
+				})
+				.catch(failure => {
+					console.log(failure);
+					reject(failure);
+				})
+		});
 	}
 
 	this.setConnectionBusy = function(connection)
 	{
-		connection.busy = true;
+		connection.active = true;
 	}
 
 	this.setConnectionNotBusy = function(connection)
 	{
-		connection.busy = false;
+		connection.active = false;
+	}
+
+	this.purgeConnection = function(connection)
+	{
+		_.forEach(pool, (dbpool, index) => {
+			pool[index].connections = _.reduce(dbpool.connections, (arry,current) => {
+				if (!(current === connection)) arry.push(current);
+				return arry;
+			}, []);
+			pool[index].counter = pool[index].connections.length;
+		});
+	}
+
+	this.purgePool = function(dbName)
+	{
+		pool[dbName].counter = 0;
+		pool[dbName].connections = [];
+		return refreshPool(dbName);
+	}
+
+	this.refreshPool = function(dbName)
+	{
+		let promise = new Promise((resolve, reject) =>
+		{
+			let promises = [];
+			let actives = _.filter(pool[dbName].connections, item => item.active == true);
+			pool[dbName].counter = actives.length;
+			pool[dbName].connections = actives;
+
+			let need = pool[dbName].maxPoolSize - pool[dbName].counter;
+
+			for (let x = 0; x < need; x++)
+			{
+				promises.push(addNewConnection(dbName));
+			}
+
+			Promise.all(promises)
+				.then(connections => resolve(connections))
+				.catch(err => {
+					console.log(err);
+					reject(err);;
+				});
+		});
+
+		return promise;
 	}
 
 	this.init = function(dbName)
@@ -93,20 +131,51 @@ function dbPool()
 				connections: []
 			};
 
-			if (process.env.DBPOOL)
-			{
-				let config = JSON.parse(process.env.DBPOOL);
+			let config = getConfig(dbName);
 
-				pool[dbName].maxPoolSize = (config[dbName].maxPoolSize != null)?config[dbName].maxPoolSize:10;
-			}
-			else
-			{
-				let config = require(__basedir + 'credentials/' + dbName + '.json');
+			pool[dbName].maxPoolSize = (config.maxPoolSize != null)?config.maxPoolSize:10;
 
-				pool[dbName].maxPoolSize = (config.maxPoolSize != null)?config.maxPoolSize:10;
-			}
-
+			return mi.refreshPool(dbName);
 		}
+
+		return Promise.resolve(pool[dbName].connections);
+	}
+
+	function getConfig(dbName)
+	{
+		if (process.env.DBPOOL)
+		{
+			return JSON.parse(process.env.DBPOOL)[dbName];
+		}
+		else
+		{
+			return require(__basedir + 'credentials/' + dbName + '.json');
+		}
+	}
+
+	function addNewConnection(dbName)
+	{
+		let promise = new Promise((resolve, reject) =>
+		{
+			var config = (process.env.DBPOOL != null)?JSON.parse(process.env.DBPOOL)[dbName]:require(__basedir + 'credentials/' + dbName + '.json');
+
+			ibmdb.open(_buildConectionString(config), function (err, conn)
+			{
+				if (err)
+				{
+					console.log(err);
+					reject(err);
+				}
+				else
+				{
+					pool[dbName].counter++;
+					pool[dbName].connections.push({ _connection: conn, active: false, id: pool[dbName].counter });
+					var dex = _.findIndex(pool[dbName].connections, { id: pool[dbName].counter });
+					resolve(pool[dbName].connections[dex]);
+				}
+			});
+		});
+		return promise;
 	}
 
 	function _buildConectionString(json)
